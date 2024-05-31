81fe29c8054a165a85b16fe9a6abe9b40ae9a6f3
jigartellis <jigar@tellistechnologies.com>
2024-05-31 14:24:37 +0600

    done

--- index.js
+++ index.js
@@ -7,14 +7,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(express.json())
 app.use(cors())
 
-// 96GUNMZLwG9pp5zO
-//employee-management
-
-
 
 const uri = `mongodb+srv://${process.env.DATABASE_USER_NAME}:${process.env.DATABASE_PASSWORD}@cluster0.sqwmnep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,8 +19,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const userCollection = client.db('employee-management').collection("users")
 
     //user post in database
@@ -144,84 +137,3 @@ app.listen(5000,()=>{
     console.log(process.env.DATABASE_USER_NAME)
     console.log('server is running port : 5000');
 })
-
-
-/*
-
- 
-
- 
-
-,
-  "payment_history":[
-  {"2023": [{"month": "jan", "salary": 2900},
-            {"month": "feb", "salary": 3900},
-            {"month": "mar", "salary": 3000},
-            {"month": "apr", "salary": 2000},
-            {"month": "may", "salary": 2780},
-            {"month": "jun", "salary": 1890},
-            {"month": "jul", "salary": 1890},
-            {"month": "aug", "salary": 1890},
-            {"month": "sep", "salary": 2390},
-            {"month": "oct", "salary": 3490},
-            {"month": "nov", "salary": 3990},
-            {"month": "dec", "salary": 3490}]
-  },{"2024": [{"month": "jan", "salary": 2900},
-            {"month": "feb", "salary": 3900}]
-  }
-]
-
-
- 
-
-
-   "year_2023":[{"month": "jan", "salary": 2900},
-              {"month": "feb", "salary": 3900},
-              {"month": "mar", "salary": 3000},
-              {"month": "apr", "salary": 2000},
-              {"month": "may", "salary": 2780},
-              {"month": "jun", "salary": 1890},
-              {"month": "jul", "salary": 1890},
-              {"month": "aug", "salary": 1890},
-              {"month": "sep", "salary": 2390},
-              {"month": "oct", "salary": 3490},
-              {"month": "nov", "salary": 3990},
-              {"month": "dec", "salary": 3490}],
-         "year_2024":[{"month": "jan", "salary": 2900},
-                {"month": "feb", "salary": 3900}
-            ]
-
- 
-
-
-            {
-  "payment_history":{
-      "transaction":true,
-      "data":{   
-        "year_2023":[{"month": "jan", "salary": 2900},
-              {"month": "feb", "salary": 3900},
-              {"month": "mar", "salary": 3000},
-              {"month": "apr", "salary": 2000},
-              {"month": "may", "salary": 2780},
-              {"month": "jun", "salary": 1890},
-              {"month": "jul", "salary": 1890},
-              {"month": "aug", "salary": 1890},
-              {"month": "sep", "salary": 2390},
-              {"month": "oct", "salary": 3490},
-              {"month": "nov", "salary": 3990},
-              {"month": "dec", "salary": 3490}],
-        "year_2024":[{"month": "jan", "salary": 2900},
-                {"month": "feb", "salary": 3900}
-            ]}
-}} 
-
-65e85ccd25a218f37f043d57
-65e85e4925a218f37f043d58
-65e85f3525a218f37f043d59
-65e85f6f25a218f37f043d5a
-65e85fcf25a218f37f043d5b
-65e8601625a218f37f043d5c
-65e8608825a218f37f043d5d
-65e8610b25a218f37f043d5e
-
-**/
